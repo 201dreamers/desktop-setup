917a661f3e099eef269ceb26f792db5742ed6a81
201dreamers <201dreamers@gmail.com>
2024-03-16 20:09:34 +0200

    fix errors in install script

--- install.sh
+++ install.sh
@@ -43,8 +43,8 @@ SSH_DIR="${TEMP_DIR}/.ssh"
 
 # CONFIG_DIR="${HOME}/.config"
 # LOCAL_DIR="${HOME}/.local"
-CONFIG_DIR="${TEMP_DIR}/config"
-LOCAL_DIR="${TEMP_DIR}/local"
+CONFIG_DIR="${TEMP_DIR}/.config"
+LOCAL_DIR="${TEMP_DIR}/.local"
 LOCAL_PACKAGES_DIR="${LOCAL_DIR}/packages"
 LOCAL_BIN_DIR="${LOCAL_DIR}/bin"
 LOCAL_DESKTOP_FILES_DIR="${LOCAL_DIR}/share/applications"
@@ -58,12 +58,13 @@ create_dir_if_dont_exist ${CONFIG_DIR}
 create_dir_if_dont_exist ${LOCAL_DIR}
 create_dir_if_dont_exist ${LOCAL_PACKAGES_DIR}
 create_dir_if_dont_exist ${LOCAL_BIN_DIR}
+create_dir_if_dont_exist ${LOCAL_DESKTOP_FILES_DIR}
 
 # ################################
 # Install packages from repository
 # ################################
 install_apt_packages() {
-    local basic_packages="vim zsh git curl wget tmux zip unzip  gcc make cmake bash-completition npm fzf"
+    local basic_packages="vim zsh git curl wget tmux zip unzip  gcc make cmake bash-completion npm fzf"
     local python_packages="python-is-python3 python3-all python3-all-venv python3-pip-whl python3-pip python3-pynvim"
     local gnome_packages="gnome-shell-extensions gnome-shell-extension-manager gnome-tweaks"
     local application_packages="synaptic dconf-editor vlc ubuntu-restricted-extras gimp libreoffice gtk-3-examples flameshot obs-studio"
@@ -80,12 +81,6 @@ install_apt_packages() {
 setup_git() {
     echo -n "Enter you email: "
     read -r email
-
-    echo -n "Enter you username: "
-    read -r username
-
-    git config --global user.name "${username}"
-    git config --global user.email "${email}"
 
     echo -n "Do you have existing git ssh keys? [y/n]: "
     read -r ans
@@ -123,18 +118,15 @@ download_package() {
 
 # $1 - app name
 # #2 - url
-# $3 - ask for new url ["yes" or empty]
+# $3 - ask for new url ["y" or empty]
 install_deb_package() {
     printf "\n=> ${1}:\n"
 
-    local url
-    if [[ "${3}" == "yes" ]]; then
+    local url=${2}
+    if [[ "${3}" == "y" ]]; then
         print_ask_url_message ${1}
-        url=$(ask_for_url ${2})
-    else
-        url=${2}
+        url=$(ask_for_url ${url})
     fi
-
     local package_name=${url##*/}
     local filepath="${DOWNLOADS_DIR}/${package_name}"
 
@@ -157,19 +149,25 @@ install_nvim() {
     ln -sf "${LOCAL_PACKAGES_DIR}/nvim-linux64/bin/nvim" "${LOCAL_BIN_DIR}/nvim"
 
     echo "Linking your local and remote configs"
-    ln -sf ./nvim-config "${CONFIG_DIR}/nvim"
+    ln -sf "${INSTALL_SCRIPT_DIR}/nvim-config" "${CONFIG_DIR}/nvim"
 }
 
 install_oh_my_zsh() {
     printf "\n=> Oh my zsh:\n"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    mv "${TEMP_DIR}/.zshrc" "${TEMP_DIR}/.zshrc_bak"
+    ln -sf "${INSTALL_SCRIPT_DIR}/zsh-config/.zshrc" "${TEMP_DIR}/.zshrc"
 }
 
+# $1 - ask for new url ["y" or empty]
 install_lazygit() {
     printf "\n=> Lazygit:\n"
 
-    print_ask_url_message ${1}
-    local url=$(ask_for_url "https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz")
+    local url="https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz"
+    if [[ "${1}" == "y" ]]; then
+        print_ask_url_message "Lazygit"
+        url=$(ask_for_url ${url})
+    fi
     local package_name=${url##*/}
     local filepath="${DOWNLOADS_DIR}/${package_name}"
 
@@ -183,16 +181,18 @@ install_lazygit() {
 install_kitty() {
     printf "\n=> Kitty:\n"
 
-    local url=$(ask_for_url "https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz")
-    local package_name=${url##*/}
-    local filepath="${DOWNLOADS_DIR}/${package_name}"
-
     echo "Running install scrip for kitty"
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin dest=${LOCAL_PACKAGES_DIR} launch=n
     ln -sf "${LOCAL_PACKAGES_DIR}/kitty.app/bin/kitty" "${LOCAL_PACKAGES_DIR}/kitty.app/bin/kitten" ${LOCAL_BIN_DIR}
     cp "${LOCAL_PACKAGES_DIR}/kitty.app/share/applications/kitty.desktop" ${LOCAL_DESKTOP_FILES_DIR}
     sed -i "s|Icon=kitty|Icon=${LOCAL_PACKAGES_DIR}/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" "${LOCAL_DESKTOP_FILES_DIR}/kitty*.desktop"
     sed -i "s|Exec=kitty|Exec=${LOCAL_PACKAGES_DIR}/kitty.app/bin/kitty|g" "${LOCAL_DESKTOP_FILES_DIR}/kitty*.desktop"
+
+    echo "Setting theme for kitty"
+    ln -sf "${INSTALL_SCRIPT_DIR}/kitty-config/kitty-themes/themes/Sundried.conf" "${INSTALL_SCRIPT_DIR}/kitty-config/theme.conf"
+
+    echo "Linking your local and remote configs"
+    ln -sf "${INSTALL_SCRIPT_DIR}/kitty-config" "${CONFIG_DIR}/kitty"
 }
 
 install_telegram() {
@@ -208,10 +208,15 @@ install_telegram() {
     ${LOCAL_PACKAGES_DIR}/Telegram/Telegram
 }
 
+# $1 - ask for new url ["y" or empty]
 install_vial() {
     printf "\n=> Vial:\n"
 
-    local url=$(ask_for_url "https://github.com/vial-kb/vial-gui/releases/download/v0.7.1/Vial-v0.7.1-x86_64.AppImage")
+    local url="https://github.com/vial-kb/vial-gui/releases/download/v0.7.1/Vial-v0.7.1-x86_64.AppImage"
+    if [[ "${1}" == "y" ]]; then
+        print_ask_url_message "Vial"
+        url=$(ask_for_url ${url})
+    fi
     local package_name=${url##*/}
     local filepath="${DOWNLOADS_DIR}/${package_name}"
 
@@ -220,35 +225,39 @@ install_vial() {
     chmod +x "${LOCAL_PACKAGES_DIR}/vial"
 
     echo "Creating .desktop file for vial"
-    cp vial.desktop ${LOCAL_DESKTOP_FILES_DIR}
+    cp "${INSTALL_SCRIPT_DIR}/desktop_files/vial.desktop" "${LOCAL_DESKTOP_FILES_DIR}"
 
     echo "Setting udev rule"
     export USER_GID=`id -g`; sudo --preserve-env=USER_GID sh -c 'echo "KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{serial}==\"*vial:f64c2b3c*\", MODE=\"0660\", GROUP=\"$USER_GID\", TAG+=\"uaccess\", TAG+=\"udev-acl\"" > /etc/udev/rules.d/99-vial.rules && udevadm control --reload && udevadm trigger'
 }
 
+# $1 - ask for url with new version
 install_other_packages() {
-    install_deb_package "Chrome" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-    install_deb_package "Ripgrep" "https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb" "yes"
-    install_deb_package "Fd" "https://github.com/sharkdp/fd/releases/download/v9.0.0/fd-musl_9.0.0_amd64.deb" "yes"
+    # install_deb_package "Chrome" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
     install_nvim
     install_oh_my_zsh
-    install_lazygit
     install_kitty
-    install_telegram
+    # install_telegram
+
+    install_deb_package "Ripgrep" "https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb" ${1}
+    install_deb_package "Fd" "https://github.com/sharkdp/fd/releases/download/v9.0.0/fd-musl_9.0.0_amd64.deb" ${1}
+
+    install_lazygit ${1}
+    install_vial ${1}
 }
 
 
-# ##################
+# ###############################
 # Functions to configure packages
-# ##################
+# ###############################
 configure_tmux() {
     printf "\n=> Tmux:\n"
     local tmux_dir="${CONFIG_DIR}/tmux"
 
     create_dir_if_dont_exist ${tmux_dir}
     echo "Linking your local and remote configs"
-    ln -sf tmux.conf ${tmux_dir}
+    ln -sf "${INSTALL_SCRIPT_DIR}/tmux-config/tmux.conf" "${tmux_dir}"
     git clone https://github.com/tmux-plugins/tpm "${tmux_dir}/plugins/tpm"
     echo "After tmux start press prefix+I to install plugins"
 }
@@ -261,7 +270,7 @@ configure_flatpak() {
 configure_corne() {
     printf "\n=> Corne:\n"
 
-    local qmk_dir="${HOME}/Documents/qmk-fw"
+    local qmk_dir="${TEMP_DIR}/Documents/qmk-fw"
     create_dir_if_dont_exist ${qmk_dir}
 
     cd ${qmk_dir}
@@ -273,11 +282,10 @@ configure_corne() {
     cd vial-qmk
     qmk setup
 
-    cd keyboards/crkbd/keymaps
-    ln -sf corne-fw keyboards/crkbd/keymaps/201dreamers
-
     deactivate
     cd ${INSTALL_SCRIPT_DIR}
+
+    ln -sf "${INSTALL_SCRIPT_DIR}/corne-fw" "${qmk_dir}/vial-qmk/keyboards/crkbd/keymaps/201dreamers"
 }
 
 configure_packages() {
@@ -291,25 +299,30 @@ configure_packages() {
 # ###########
 printf "\n====================\n"
 echo "Hi, this is your local Jarvis, I'll help you to install the environment"
+echo "Updating all packages"
+echo "Please, be ready to input your password. Press Enter now"
+read
+sudo apt update && sudo apt upgrade -y
 
 printf "\n====================\n"
 echo "Installing packages from repository"
 install_apt_packages
 
-printf "\n\n====================\n"
+printf "\n====================\n"
 echo "Setting up Git"
 setup_git
 
-printf "\n\n====================\n"
+printf "\n====================\n"
 echo "Installing packages from other sources"
-install_other_packages
+echo -n "Do you want to provide urls for new versions of packages? [y/n]: "
+read -r ans
+install_other_packages ${ans}
 
-printf "\n\n====================\n"
+printf "\n====================\n"
 echo "Configuring packages"
 configure_packages
 
-printf "\n\n====================\n"
-
+printf "\n====================\n"
 echo -n "Do you want to turn off Ubuntu reporting? [y/n]: "
 read -r ans
 if [[ "${ans}" == "y" ]]; then
@@ -321,3 +334,5 @@ read -r ans
 if [[ "${ans}" == "y" ]]; then
     reboot
 fi
+
+# rm -rf ${TEMP_DIR}
